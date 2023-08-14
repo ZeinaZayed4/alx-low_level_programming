f44a291a983ef41ef4f12f342f6d702075d93883
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2023-08-14 13:38:56 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 48;
+
+while (x <= 57)
+{
+putchar((int)x);
+x++;
+}
+
+putchar('\n');
+
+return (0);
+}
