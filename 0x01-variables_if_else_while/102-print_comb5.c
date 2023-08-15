78ee7172934d75871c37ebb8374156b3065cb5f2
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2023-08-14 17:24:32 -0700

    print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int c = 48;
+
+while (c <= 57)
+{
+int n = 48;
+
+while (n <= 57)
+{
+int m = n + 1;
+
+while (m <= 57)
+{
+int r = 48;
+
+while (r <= 57)
+{
+putchar(c);
+putchar(n);
+putchar(' ');
+putchar(m);
+putchar(r);
+
+if (c != 57 || n != 56 || m != 57 || r != 57)
+{
+putchar(',');
+putchar(' ');
+}
+r++;
+}
+m++;
+}
+n++;
+}
+c++;
+putchar('\n');
+}
+return (0);
+}
